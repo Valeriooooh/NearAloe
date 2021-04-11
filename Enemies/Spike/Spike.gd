a9b91dcfc2d444fb1d1327c3652e07ba6c59b32e
Valeriooooh <diogocsvalerio@protonmail.com>
2021-04-11 23:31:19 +0100

    ver 2.3, no player particles, SpikeBoss

--- Enemies/Spike/Spike.gd
+++ Enemies/Spike/Spike.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+export var EnemyDeathEffect:PackedScene
 
 enum {
 	IDLE,
@@ -10,7 +10,8 @@ enum {
 
 export var ACCELERATION = 0
 export var MAX_SPEED = 0
-
+export(bool) var dropHeart = false
+export var heart:PackedScene
 var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 export var FRICTION = 200
@@ -75,6 +76,10 @@ func _on_Stats_no_health():
 	var enemyDeathEffect = EnemyDeathEffect.instance()
 	get_parent().add_child(enemyDeathEffect)
 	enemyDeathEffect.global_position = global_position - Vector2(0, 8)
+	if dropHeart:
+		var Heart = heart.instance()
+		get_parent().call_deferred("add_child",Heart)
+		Heart.global_position = global_position - Vector2(0,5)
 
 func state_timer():
 	if wanderController.get_time_left() == 0:
@@ -82,8 +87,8 @@ func state_timer():
 		wanderController.start_wander_timer(rand_range(1,3))
 
 func _on_Hurtbox_invincibility_ended() -> void:
-	animationPlayer.play("Stop")
+	animationPlayer.call_deferred("play","Stop")
 
 
 func _on_Hurtbox_invincibility_started() -> void:
-	animationPlayer.play("Start")
+	animationPlayer.call_deferred("play","Start")
